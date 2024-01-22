c4a92bdece72c8e7cf5e04273d79f1cf6d2a1663
tianming <tianmingjr@dhcc.com.cn>
2024-01-22 11:57:15 +0800

    删除代码

--- src/pages/newpipeline/schedules.tsx
+++
@@ -1,8 +0,0 @@
-
-import { Button } from "rsuite";
-const Schedules = () => {
-    return <>
-        <Button color="green" appearance="primary" style={{ marginTop: 10 }}>Create Pipeline</Button>
-    </>
-}
-export default Schedules;
